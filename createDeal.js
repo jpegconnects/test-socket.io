4a322dd866fd290204ad9c2a05ef196049f1cff8
Jesuan <jesuan.patermin@egconnects.com>
2025-01-09 11:29:08 -0400

    Agregando el id del Deal en la creacion del deal

--- createDeal.js
+++ createDeal.js
@@ -23,10 +23,10 @@ router.post('/create-deal', async (req, res) => {
 
         if(responseDeal.data.result.length) {
 
-            return res.send({creado: false, existe: true})
+            return res.send({creado: false, existe: true, deal: responseDeal.data.result[0].ID})
         }
     
-        await axios.post('https://demo-egconnects.bitrix24.com/rest/221/t9a366b47rs3tas0/crm.deal.add.json', {
+        let DealExist = await axios.post('https://demo-egconnects.bitrix24.com/rest/221/t9a366b47rs3tas0/crm.deal.add.json', {
                 fields: {
                     TITLE: title,
                     CATEGORY_ID: category
@@ -39,7 +39,7 @@ router.post('/create-deal', async (req, res) => {
             }
         )
     
-        return res.send({creado: true, existe: false})
+        return res.send({creado: true, existe: false, deal: DealExist.data.result[0].ID})
     } catch (error) {
         console.log(error)
     }
